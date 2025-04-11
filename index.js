31573d14b5d42f35ea713d21684ef6715a25b6f2
Mitali-Ghag <136098148+Mitali-Ghag@users.noreply.github.com>
2025-04-11 18:49:45 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,48 +1,10 @@
-require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const authRoutes = require("./routes/authRoutes");
-const courseRoutes = require("./routes/courseRoutes");
-const enrollmentRoutes = require("./routes/enrollmentRoutes");
-const app = express();
-const PORT = process.env.PORT || 5000;
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+import App from './App';
 
-// Middleware
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
-app.use(express.json());
-app.use("/uploads", express.static("uploads"));
-
-// Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/courses", courseRoutes);
-app.use("/api/enrollments", enrollmentRoutes);
-
-// MongoDB Connection
-const connectDB = async () => {
-  try {
-    if (!process.env.MONGO_URL) {
-      throw new Error("MONGO_URL is missing in .env file!");
-    }
-
-    await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    console.log("✅ MongoDB Connected");
-  } catch (error) {
-    console.error("❌ MongoDB Connection Failed:", error.message);
-    return process.exit(1);
-  }
-};
-
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
-  });
-});
-
-app.get("/", (req, res) => {
-  res.send("🔥 API is running...");
-});
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
+);
